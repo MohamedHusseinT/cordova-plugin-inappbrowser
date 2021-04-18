9e79c80db8dfa2292f1d3542dba025c0a0bd973c
Mohamed <MohamedHusseinT@users.noreply.github.com>
2021-04-18 15:28:24 +0200

    Update CDVInAppBrowserOptions.m

--- src/ios/CDVInAppBrowserOptions.m
+++ src/ios/CDVInAppBrowserOptions.m
@@ -45,6 +45,8 @@
         self.toolbarcolor = nil;
         self.toolbartranslucent = YES;
         self.beforeload = @"";
+     
+        self.OverrideUserAgentOption = nil;
     }
 
     return self;
